2308d526269dcdd0e9bba4cef05d5b8e68d843a3
eerussianguy <eerussianguy@gmail.com>
2024-08-21 21:05:01 -0400

    rm processresources

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,30 +93,7 @@ neoForge {
 
 tasks {
     processResources {
-        if (modIsInCI) {
-            doLast {
-                val jsonMinifyStart: Long = System.currentTimeMillis()
-                var jsonMinified: Long = 0
-                var jsonBytesBefore: Long = 0
-                var jsonBytesAfter: Long = 0
-
-                fileTree(mapOf("dir" to outputs.files.asPath, "include" to "**/*.json")).forEach {
-                    jsonMinified++
-                    jsonBytesBefore += it.length()
-                    try {
-                        it.writeText(JsonOutput.toJson(JsonSlurper().parse(it)).replace("\"__comment__\":\"This file was automatically created by mcresources\",", ""))
-                    } catch (e: Exception) {
-                        println("JSON Error in ${it.path}")
-                        throw e
-                    }
-
-                    jsonBytesAfter += it.length()
-                }
-                println("Minified $jsonMinified json files. Reduced ${jsonBytesBefore / 1024} kB to ${(jsonBytesAfter / 1024)} kB. Took ${System.currentTimeMillis() - jsonMinifyStart} ms")
-            }
-        }
     }
-
 
     jar {
         manifest {
